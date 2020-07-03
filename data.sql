104505f35e4fdcae6dc1c387d1e509b37f651a06
barajas-d <diegobarajas102030@gmail.com>
2020-07-03 10:24:42 -0500

    update

--- data.sql
+++ data.sql
@@ -1,27 +1,9 @@
 CREATE DATABASE IF NOT EXISTS ballot_chain;
 
 DROP TABLE IF EXISTS employess;
-DROP TABLE IF EXISTS votacion;
 DROP TABLE IF EXISTS opcion;
+DROP TABLE IF EXISTS votacion;
 
-CREATE TABLE employess(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(200) DEFAULT NULL,
-    salary INT DEFAULT NULL,
-    PRIMARY KEY(id)
-);
-
-INSERT INTO employess (name, salary) values ('pepito', 10000);
-INSERT INTO employess (name, salary) values ('juanito', 20000);
-INSERT INTO employess (name, salary) values ('juanito', 300000);
-
-
-CREATE TABLE opcion(
-    id INT NOT NULL AUTO_INCREMENT,
-    descripcion VARCHAR(200) DEFAULT NULL,
-    nombre VARCHAR(200) DEFAULT NULL,
-    PRIMARY KEY(id)
-);
 
 CREATE TABLE votacion(
     id INT NOT NULL AUTO_INCREMENT,
@@ -30,15 +12,24 @@ CREATE TABLE votacion(
     tipoDeVotacion INT NOT NULL,
     descripcion VARCHAR(200) DEFAULT NULL,
     votos INT DEFAULT 1,
-    PRIMARY KEY(id),
-    FOREIGN KEY(tipoDeVotacion) REFERENCES opcion(id)
+    PRIMARY KEY(id)
 );
 
+CREATE TABLE opcion(
+    id INT NOT NULL AUTO_INCREMENT,
+    idVotacion INT NOT NULL,
+    identificacion VARCHAR(200) DEFAULT NULL, 
+    descripcion VARCHAR(200) DEFAULT NULL,
+    nombre VARCHAR(200) DEFAULT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (idVotacion) REFERENCES votacion(id)
+);
 
-INSERT INTO opcion (id, nombre, descripcion) VALUES (1, 'Popular', 'Opcion de ejemplo');
-INSERT INTO opcion (id, nombre, descripcion) VALUES (2, 'Ranking', 'Opcion de ejemplo');
-INSERT INTO opcion (id, nombre, descripcion) VALUES (3, 'Clasificación', 'Opcion de ejemplo');
 
 INSERT INTO votacion (fechaLimite, tipoDeVotacion, descripcion) VALUES ('2020-10-20', 1, 'ejemplo votacion popular');
 INSERT INTO votacion (fechaLimite, tipoDEVotacion, descripcion) VALUES ('2020-10-20', 1, 'ejemplo votacion ranking');
 INSERT INTO votacion (fechaLimite, tipoDEVotacion, descripcion) VALUES ('2020-10-20', 1, 'ejemplo votacion clasificacion');
+
+INSERT INTO opcion (idVotacion, nombre, descripcion, identificacion) VALUES (1, 'candidato 1', 'Descripcion', '123');
+INSERT INTO opcion (idVotacion, nombre, descripcion, identificacion) VALUES (1, 'candidato 2', 'Descripcion', '456');
+INSERT INTO opcion (idVotacion, nombre, descripcion, identificacion) VALUES (2, 'candidato 1', 'Descripcion', '789');
