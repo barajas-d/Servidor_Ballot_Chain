06900938a79ebff0cc27141c390995384b34060c
Bridamo98 <bridamo.98@gmail.com>
2020-07-26 23:00:56 -0500

    Validacion en todos los servicios de grupo

--- src/routes/grupo.js
+++ src/routes/grupo.js
@@ -15,7 +15,7 @@ router.get('/log', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     })
 });
 
-router.get('/grupo', cors(corsOptionsDelegate), (req, res) => {
+router.get('/grupo', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     mysqlConnection.query('SELECT * FROM grupo', (err, rows) => {
         if(!err){
             res.json(rows);
@@ -26,7 +26,7 @@ router.get('/grupo', cors(corsOptionsDelegate), (req, res) => {
     })
 });
 
-router.get('/grupo/:id',cors(corsOptionsDelegate), (req, res) => {
+router.get('/grupo/:id', verificarToken,cors(corsOptionsDelegate), (req, res) => {
     const { id } = req.params;
     mysqlConnection.query('SELECT * FROM grupo WHERE id = ?', [id],  (err, rows, fields) => {
         if(!err){
@@ -39,7 +39,7 @@ router.get('/grupo/:id',cors(corsOptionsDelegate), (req, res) => {
     })
 });
 
-router.post('/grupo',cors(corsOptionsDelegate), (req, res) => {
+router.post('/grupo', verificarToken,cors(corsOptionsDelegate), (req, res) => {
     const { nombre, descripcion, creador } = req.body;
     const query = "INSERT INTO grupo (nombre, descripcion, creador) VALUES (?, ?, ?)";
     mysqlConnection.query(query, [nombre, descripcion, creador], (err, rows, fields) => {
@@ -51,7 +51,7 @@ router.post('/grupo',cors(corsOptionsDelegate), (req, res) => {
     });
 });
 
-router.delete('/grupo/:id',cors(corsOptionsDelegate), (req, res) => {
+router.delete('/grupo/:id', verificarToken,cors(corsOptionsDelegate), (req, res) => {
     const { id } = req.params;
     const query = "DELETE FROM grupo WHERE ? = id";
     mysqlConnection.query(query, [id], (err, rows) => {
@@ -68,7 +68,7 @@ router.delete('/grupo/:id',cors(corsOptionsDelegate), (req, res) => {
     })
 });
 
-router.put('/grupo', cors(corsOptionsDelegate), (req, res) => {
+router.put('/grupo', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     const { id, nombre, descripcion, creador } = req.body;
     const query = "UPDATE grupo SET nombre = ?, descripcion = ?, creador = ? WHERE id = ?";
     mysqlConnection.query(query, [nombre, descripcion, creador, id], (err, rows, fields) => {
@@ -85,7 +85,7 @@ router.put('/grupo', cors(corsOptionsDelegate), (req, res) => {
     });
 });
 
-router.get('/usuario',cors(corsOptionsDelegate), (req, res) => {//esto es de usuario
+router.get('/usuario', verificarToken,cors(corsOptionsDelegate), (req, res) => {//esto es de usuario
     mysqlConnection.query('SELECT * FROM usuario', (err, rows) => {
         if(!err){
             res.json(rows);
@@ -96,7 +96,7 @@ router.get('/usuario',cors(corsOptionsDelegate), (req, res) => {//esto es de usu
     })
 });
 
-router.get('/usuario/:nombre',cors(corsOptionsDelegate), (req, res) => {//esto es de usuario
+router.get('/usuario/:nombre', verificarToken,cors(corsOptionsDelegate), (req, res) => {//esto es de usuario
     const { nombre } = req.params;
     mysqlConnection.query('SELECT * FROM usuario WHERE nombre = ?', [nombre],  (err, rows, fields) => {
         if(!err){
@@ -108,7 +108,7 @@ router.get('/usuario/:nombre',cors(corsOptionsDelegate), (req, res) => {//esto e
     })
 });
 
-router.get('/miembro/grupo/:id',cors(corsOptionsDelegate), (req, res) => {
+router.get('/miembro/grupo/:id', verificarToken,cors(corsOptionsDelegate), (req, res) => {
     const { id } = req.params;
     mysqlConnection.query('SELECT * FROM usuario AS t1 INNER JOIN miembro AS t2 WHERE t1.nombre = t2.idUsuario AND t2.idGrupo = ?', [id],  (err, rows, fields) => {
         if(!err){
@@ -120,7 +120,7 @@ router.get('/miembro/grupo/:id',cors(corsOptionsDelegate), (req, res) => {
     })
 });
 
-router.post('/miembro', cors(corsOptionsDelegate), (req, res) => {
+router.post('/miembro', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     const { idUsuario, idGrupo} = req.body;
     const query = "INSERT INTO miembro (idUsuario, idGrupo) VALUES (?, ?)";
     mysqlConnection.query(query, [idUsuario, idGrupo], (err, rows, fields) => {
@@ -132,7 +132,7 @@ router.post('/miembro', cors(corsOptionsDelegate), (req, res) => {
     });
 });
 
-router.delete('/miembro/:idUsuario/:idGrupo',cors(corsOptionsDelegate), (req, res) => {
+router.delete('/miembro/:idUsuario/:idGrupo', verificarToken,cors(corsOptionsDelegate), (req, res) => {
     const { idUsuario, idGrupo } = req.params;
     const query = "DELETE FROM miembro WHERE ? = idUsuario AND ? = idGrupo";
     mysqlConnection.query(query, [idUsuario, idGrupo], (err, rows) => {
@@ -149,7 +149,7 @@ router.delete('/miembro/:idUsuario/:idGrupo',cors(corsOptionsDelegate), (req, re
     })
 });
 
-router.delete('/miembro/:id', cors(corsOptionsDelegate), (req, res) => {
+router.delete('/miembro/:id', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     const {id } = req.params;
     const query = "DELETE FROM miembro WHERE ? = idGrupo";
     mysqlConnection.query(query, [id], (err, rows) => {
@@ -166,7 +166,7 @@ router.delete('/miembro/:id', cors(corsOptionsDelegate), (req, res) => {
     })
 });
 
-router.get('/pendiente/grupo/:id', cors(corsOptionsDelegate), (req, res) => {
+router.get('/pendiente/grupo/:id', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     const { id } = req.params;
     mysqlConnection.query('SELECT * FROM usuario AS t1 INNER JOIN pendiente AS t2 WHERE t1.nombre = t2.idUsuario AND t2.idGrupo = ?', [id],  (err, rows, fields) => {
         if(!err){
@@ -178,7 +178,7 @@ router.get('/pendiente/grupo/:id', cors(corsOptionsDelegate), (req, res) => {
     })
 });
 
-router.post('/pendiente', cors(corsOptionsDelegate), (req, res) => {
+router.post('/pendiente', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     const { idUsuario, idGrupo} = req.body;
     const query = "INSERT INTO pendiente (idUsuario, idGrupo) VALUES (?, ?)";
     mysqlConnection.query(query, [idUsuario, idGrupo], (err, rows, fields) => {
@@ -190,7 +190,7 @@ router.post('/pendiente', cors(corsOptionsDelegate), (req, res) => {
     });
 });
 
-router.delete('/pendiente/:idUsuario/:idGrupo', cors(corsOptionsDelegate), (req, res) => {
+router.delete('/pendiente/:idUsuario/:idGrupo', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     const { idUsuario, idGrupo } = req.params;
     const query = "DELETE FROM pendiente WHERE ? = idUsuario AND ? = idGrupo";
     mysqlConnection.query(query, [idUsuario, idGrupo], (err, rows) => {
@@ -207,7 +207,7 @@ router.delete('/pendiente/:idUsuario/:idGrupo', cors(corsOptionsDelegate), (req,
     })
 });
 
-router.delete('/pendiente/:id', cors(corsOptionsDelegate), (req, res) => {
+router.delete('/pendiente/:id', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     const {id } = req.params;
     const query = "DELETE FROM pendiente WHERE ? = idGrupo";
     mysqlConnection.query(query, [id], (err, rows) => {
@@ -227,18 +227,17 @@ router.delete('/pendiente/:id', cors(corsOptionsDelegate), (req, res) => {
 module.exports = router;
 
 function verificarToken(req, res, next) {
-    console.log(req.headers.authorization);
+    console.log(req.headers);
     if (!req.headers.authorization) {
+        console.log("primer if");
         return res.status(401).send('Solicitud no autorizada');
     }
     const token = req.headers.authorization.split(' ')[1];
     if (token === 'null') {
+        console.log("segundo if");
         return res.status(401).send('Solicitud no autorizada');
     }
-    //console.log('token: '+ token)
-
     const datos = jwt.verify(token, secretKey)
-    //console.log(datos);
     req.userId = datos._id;
     next();
 }
