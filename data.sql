ead28181245984b95177035674d2c7e3f4ca366a
BrandonnC9898 <brandonn9898@gmail.com>
2020-08-11 09:19:57 -0500

    Contraseñas de Brandonn y Alice encriptadas

--- data.sql
+++ data.sql
@@ -136,9 +136,9 @@ INSERT INTO credencial (id, clave, isValid, votacion) VALUES(2,'def',True, 2);
 INSERT INTO credencial (id, clave, isValid, votacion) VALUES(3,'ghi',True, 3);
 INSERT INTO credencial (id, clave, isValid, votacion) VALUES(4,'jkl',False, 1);
 
-INSERT INTO usuario (nombre, correo, contrasena) VALUES ('Alice', 'alice@gmail.com', '123');
+INSERT INTO usuario (nombre, correo, contrasena) VALUES ('Alice', 'alice@gmail.com', '202cb962ac59075b964b07152d234b70');
 INSERT INTO usuario (nombre, correo, contrasena) VALUES ('Bob', 'bob@gmail.com', '123');
-INSERT INTO usuario (nombre, correo, contrasena) VALUES ('Brandonn', 'brandonn@gmail.com', '123');
+INSERT INTO usuario (nombre, correo, contrasena) VALUES ('Brandonn', 'brandonn@gmail.com', '202cb962ac59075b964b07152d234b70');
 
 INSERT INTO usuario (nombre, correo) VALUES 
 ('Usuario1','correo 1'),
