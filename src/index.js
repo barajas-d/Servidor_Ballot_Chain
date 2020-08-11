56c09e4cdef4130fd855af405720a648292a4343
Briam Daniel Agudelo Mogollón <bridamo.98@gmail.com>
2020-08-11 04:05:12 -0500

    primer_commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const { ExpressPeerServer } = require('peer');
 const app = express();
+const httpserver = require('http').createServer(app);
 const cors = require('cors');
+
+const peerServer = ExpressPeerServer(httpserver, {
+    debug: true,
+    path: '/'
+});
  
+const peer_port = process.env.PORT || 9000;
+
+httpserver.listen(peer_port, function () {//Poner servidor en modo de escucha para responder a peticiones HTTP
+    console.log("listening peer connections on : " + peer_port);
+});
 
 //Configuracion acceso
 app.set('port', process.env.PORT || 3000);
@@ -9,6 +21,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(express.json());
 //Rutas
+app.use('/peerjs', peerServer);
 app.use(require('./routes/employees'));
 app.use(require('./routes/votacion'));
 app.use(require('./routes/grupo'));
@@ -20,4 +33,12 @@ app.use(require('./routes/tipoVotacion'));
 //Iniciar
 app.listen(3000, () =>{
     console.log('Server on port', app.get('port'))
+});
+
+peerServer.on('connection', (client) => {
+    console.log("Peer conectado");
+});
+
+peerServer.on('disconnect', (client) => {
+    console.log("Peer desconectado")
 });
