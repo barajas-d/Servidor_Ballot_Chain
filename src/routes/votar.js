1e968a10d0e35e6c2a66304c2e47f59050538f4d
barajas-d <diegobarajas102030@gmail.com>
2020-09-08 11:45:27 -0500

    Validad usuarios

--- src/routes/votar.js
+++ src/routes/votar.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 const mysqlConnection = require('../dataBase');
 const corsOptionsDelegate = require('../cors');
 const cifrado = require('./../cifrado');
+const jwt = require('jsonwebtoken');
 
-router.post('/redirigir', cors(corsOptionsDelegate), (req, res) => {
+const secretKey = '123456789'
+
+router.post('/redirigir', verificarToken, cors(corsOptionsDelegate), (req, res) => {
     const { voto, firma, peerValidador, encryptId } = req.body;
 
     //console.log("mensaje: " + voto);
@@ -21,5 +24,21 @@ router.post('/redirigir', cors(corsOptionsDelegate), (req, res) => {
     
     res.json({Status: "Voto redirigido"});
 });
+
+function verificarToken(req, res, next) {
+    console.log(req.headers);
+    if (!req.headers.authorization) {
+        console.log("primer if");
+        return res.status(401).send('Solicitud no autorizada');
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    if (token === 'null') {
+        console.log("segundo if");
+        return res.status(401).send('Solicitud no autorizada');
+    }
+    const datos = jwt.verify(token, secretKey)
+    req.userId = datos._id;
+    next();
+}
 
 module.exports = router;
