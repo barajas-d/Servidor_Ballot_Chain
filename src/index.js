b3c04e9e5ae82a62bd9d97eb07bbc93d6bbb617b
barajas-d <diegobarajas102030@gmail.com>
2020-11-01 17:06:57 -0500

    https server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const fs = require('fs');
 const { ExpressPeerServer } = require('peer');
 const app = express();
-const httpserver = require('http').createServer(app);
+const httpsServer = require('https');
+//const httpserver = require('http').createServer(app);
 const cors = require('cors');
 const validadores = require('./validadores');
 const server = require('http').createServer(app);
@@ -57,31 +59,36 @@ io.on('disconnect', () => {
     console.log('Algo se desconecto');
 });
 
-const listenerPort = process.env.PORTLISTENER || 4000;
-server.listen(listenerPort, function(){
-    console.log("Socket listening on port : " + listenerPort);
+//INICIO SOCKET HTTPS
+const socketPort = process.env.PORTSOCKET || 4000;
+httpsServer.createServer({
+    key: fs.readFileSync('ssl.key/private.key'),
+    cert: fs.readFileSync('ssl.crt/certificate.crt')
+}, app).listen(socketPort, function(){
+console.log("Socket https server listening on port " + socketPort + "...");
 });
 //Fin Listener por socket.io
 
 
-const peerServer = ExpressPeerServer(httpserver, {
+const peerServer = ExpressPeerServer(httpsServer, {
     debug: true,
     path: '/'
 });
  
 const peer_port = process.env.PORTPEER || 5000;
-
-httpserver.listen(peer_port, function () {
-    console.log("listening peer connections on : " + peer_port);
+httpsServer.createServer({
+    key: fs.readFileSync('ssl.key/private.key'),
+    cert: fs.readFileSync('ssl.crt/certificate.crt')
+}, app).listen(peer_port, function(){
+console.log("Peer https server listening on port " + peer_port + "...");
 });
 
-//Configuracion acceso
-app.set('port', process.env.PORT || 3000);
+
 //Middlewares
 app.use(cors());
 app.use(express.json());
 //PeerJs
-app.use('/peerjs', peerServer);
+//app.use('/peerjs', peerServer);
 //Rutas
 app.use(require('./routes/employees'));
 app.use(require('./routes/votacion'));
@@ -95,11 +102,15 @@ app.use(require('./routes/validador'));
 app.use(require('./routes/votar'));
 app.use(require('./routes/resultados'));
 app.use(require('./routes/alias'));
-//Iniciar
-app.listen(3000, () =>{
-    console.log('Server on port', app.get('port'))
+//Configuracion acceso e inicio REST
+const rest_port = process.env.PORT || 3000;
+httpsServer.createServer({
+    key: fs.readFileSync('ssl.key/private.key'),
+    cert: fs.readFileSync('ssl.crt/certificate.crt')
+}, app).listen(rest_port, function(){
+console.log("REST https server listening on port " + rest_port + "...");
 });
-
+//--FIN Configurar acceso e inicio REST
 peerServer.on('connection', (client) => {
     console.log('Validador conectado');
     validadores.registrarValidador(client.getId());
